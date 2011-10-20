c20952b7c4aff7ed0d5b98e8653b2e3f02c1816a
Peter Nitsch <peter.nitsch@gmail.com>
2011-10-20 11:22:41 -0400

    removing old test example

--- examples/external.js
+++ examples/external.js
@@ -1,8 +1,9 @@
 var carrot2 = require('../lib/carrot2');
 
-// Create DCS server in debug mode
+// Create DCS server object in debug mode
 var dcs = new carrot2.DocumentClusteringServer( {debug:true} );
 
+//
 dcs.externalCall(
 	"seattle", 
 	{source:"bing-web"}, 
